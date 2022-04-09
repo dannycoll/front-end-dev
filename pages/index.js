eef51b055d21333c6c2ac9226548d5919d0800d2
Daniel Collins <dptc98@gmail.com>
2022-04-09 23:15:52 +0100

    build fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Navbar from "../components/Navbar/navbar";
+import Navbar from "../components/Navbar/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
